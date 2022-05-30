31f8348bec0d52289cd65cffe5e189eb40464cf7
GalaktycznyPaladyn <piotrmaciej.j@gmail.com>
2022-05-30 21:14:26 +0200

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterLink, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameRoomComponent } from './game-room/game-room.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
+    MenuComponent,
     AppComponent,
     GameRoomComponent,
     
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
